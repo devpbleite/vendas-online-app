d39a91a463ec3edc577b61958a547d499a9435a3
Pablo Leite <pblinux@pop-os.localdomain>
2023-05-04 21:08:55 -0300

    feat: push the project

---
+++ src/modules/login/screens/Login.tsx
@@ -0,0 +1,21 @@
+import { View } from "react-native";
+
+import Button from "../../../shared/components/button/Button";
+import Input from "../../../shared/components/input/Input";
+import { ContainerLogin } from "../styles/login.style";
+
+const Login = () => {
+  const handleOnPress = () => {
+    console.log("clicou");
+  };
+  return (
+    <View>
+      <ContainerLogin>
+        <Input />
+        <Button margin="16px" title="ENTRAR" onPress={handleOnPress} />
+      </ContainerLogin>
+    </View>
+  );
+};
+
+export default Login;
---
+++ src/shared/components/button/Button.tsx
@@ -0,0 +1,20 @@
+import { TouchableOpacityProps } from "react-native";
+
+import { theme } from "../../themes/theme";
+import Text from "../text/Text";
+import { ButtonContainer } from "./button.style";
+
+interface ButtonProps extends TouchableOpacityProps {
+  title: string;
+  margin?: string;
+}
+
+const Button = ({ title, margin, ...props }: ButtonProps) => {
+  return (
+    <ButtonContainer margin={margin} {...props}>
+      <Text color={theme.colors.neutralTheme.white}>{title}</Text>
+    </ButtonContainer>
+  );
+};
+
+export default Button;
